1697fa876787839823c4b191e0b0f4bedf4f7f40
Glitch (glitch-hello-node) <none>
2022-10-19 12:09:51 +0000

    🐙🌊 Checkpoint ./komutlar/davet.js:49968291/457

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
 let embed = new Discord.MessageEmbed()
-.setDescription('Beni Davet Etmek İçin Link: (https://discord.com/api/oauth2/authorize?client_id=970727729173512244&permissions=8&scope=bot)')
+.setDescription('**» Botun davet linki -->** [Tıkla ve davet et!](https://discord.com/api/oauth2/authorize?client_id=970727729173512244&permissions=8&scope=bot)')
+.setImage("https://media.tenor.com/vcjRrrH68p8AAAAC/shinsu-sword.gif")
 message.channel.send(embed)
 };
 exports.conf = {
