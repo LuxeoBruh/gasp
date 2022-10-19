6d2499e8eccec1e1a0475194a908e7bd298cb3a6
Glitch (glitch-hello-node) <none>
2022-10-19 08:34:35 +0000

    👨‍❤️‍👨😬 Checkpoint ./events/ready.js:49968291/1507

--- events/ready.js
+++ events/ready.js
@@ -1,25 +1,24 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 
 var prefix = ayarlar.prefix;
 
-module.exports = client => {
-var oyun = [
- 'Aeroix Bot | Developed by Gasp',
- 'Prefix : "."'
- 
-    ];
+module.exports = (client) => {
+  var oyun = [
+    "Aeroix Bot | Developed by Gasp",
+    'Prefix : "."',
+    "Jelibonu cok seviyorum 😊",
+  ];
 
-    setInterval(function() {
+  setInterval(function () {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+    client.user.setActivity(oyun[random], `${client.users.size} Users`);
+  }, 2 * 2500);
 
-        client.user.setActivity(oyun[random], `${client.users.size} Users` );
-        }, 2 * 2500);
-    
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Bot aktif edildi!`);
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] Bot aktif edildi!`);
   client.user.setStatus("online");
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Komutlar yüklendi!`);
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] Komutlar yüklendi!`);
 };
