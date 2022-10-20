be6f13c961bb69475c96518d232687ca6bf8893d
Glitch (glitch-hello-node) <none>
2022-10-20 10:54:12 +0000

    💖🚡 Checkpoint ./komutlar/animal.js:24618631/698

--- komutlar/animal.js
+++ komutlar/animal.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
 
 
-let replies = ["https://thumbs.gfycat.com/VeneratedHoarseBarebirdbat-size_restricted.gif","https://img.webme.com/pic/g/giflerderyasi/18.gif","https://media.tenor.com/ZQ98ZRr_c68AAAAC/dog-play.gif",""];
+let replies = ["https://thumbs.gfycat.com/VeneratedHoarseBarebirdbat-size_restricted.gif","https://img.webme.com/pic/g/giflerderyasi/18.gif","https://media.tenor.com/ZQ98ZRr_c68AAAAC/dog-play.gif","https://c.tenor.com/9kmaECKIx4cAAAAC/kuvet-banyo.gif","https://media.tenor.com/ZhfMGWrmCTcAAAAM/cute-kitty-best-kitty.gif","https://media.tenor.com/S32ZfhsF65oAAAAd/cute-love.gif","https://media.tenor.com/609ISt84COwAAAAd/funny-animals-cats.gif","https://media.tenor.com/xJ0Gdn_ZxMcAAAAd/cute-animals-dog.gif","https://media.tenor.com/8iVuA_QQmfoAAAAM/cute-cute-cat.gif","https://media.tenor.com/uDQjYWStzGQAAAAM/cute-funny-animals.gif","https://media.tenor.com/sTLtxR40RAwAAAAM/happymonday-cute.gif","https://media.tenor.com/GhJM_yIMd6AAAAAM/dog-tiger.gif","https://media.tenor.com/KSSvty1vP1MAAAAM/puppy-animal.gif","https://media.tenor.com/lhP24j2BooQAAAAM/bunny-flop.gif"];
 let result = Math.floor((Math.random() * replies.length));
 
 let gifembed = new Discord.MessageEmbed()
