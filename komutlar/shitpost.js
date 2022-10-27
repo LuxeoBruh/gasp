aaf843dc1b5c4474765a309f3953e6e0f6430cb1
Glitch (glitch-hello-node) <none>
2022-10-27 11:06:14 +0000

    🏩👷 Checkpoint ./komutlar/animal.js:24618631/12 ./komutlar/shitpost.js:24618631/2891

---
+++ komutlar/shitpost.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let replies = []
+let result = Math.floor((Math.random() * replies.length));
+
+let gifembed = new Discord.MessageEmbed()
+
+.setTitle("Sorumluluk bana ait değil ;")
+.setColor("RANDOM")
+.setFooter(`${message.author.tag} `, message.author.avatarURL)
+
+.setImage(replies[result]);
+message.channel.send(gifembed);
+
+
+
+};
+
+
+
+exports.conf = {
+
+  enabled: true,
+  guildOnly: false,
+  aliases: ['shitpost'],
+  permLevel: 0
+
+
+
+};
+
+exports.help = {
+  name: 'shitpost',
+  description: 'cok tehlikeli',
+  usage: 'shitpost'
+
+
+};
