8302e9fb93dc9e1618fb7d3044a95646087f77eb
Glitch (glitch-hello-node) <none>
2022-10-27 19:24:31 +0000

    🏋🛤 Checkpoint ./komutlar/shitpost.js:24618631/114

--- komutlar/shitpost.js
+++ komutlar/shitpost.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-let replies = ["https://cdn.discordapp.com/attachments/910255119642284072/1034941110566535359/301993737_464237818931738_6999490214426197358_n.mp4"]
+let replies = ["https://yt3.ggpht.com/qT1R6muMdZv158JCQX7HCKXpNsxijYRQSjy_5h7-doYCCWYob3C06QA9rvYecX1JRisX-8-OWoYgMA=s640-nd-v1","https://cdn.discordapp.com/attachments/910255119642284072/1034941110566535359/301993737_464237818931738_6999490214426197358_n.mp4"]
 
 let result = Math.floor((Math.random() * replies.length));
 
