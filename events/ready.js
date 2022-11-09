56c07288c02980c04e160fde1105b43dae724458
Glitch (glitch-hello-node) <none>
2022-11-09 20:55:58 +0000

    🚔🙈 Checkpoint ./events/ready.js:24618631/1

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = (client) => {
   var oyun = [
     "Aeroix Bot | Developed by Gasp",
     'Prefix : "."',
-  ];t
+  ];
 
   setInterval(function () {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
